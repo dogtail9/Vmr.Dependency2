21627954205194e2f8b7428f8ae3e415c7fff9a9
Christer Eriksson <christer@dogtail.se>
2023-02-28 17:34:43 +0100

    Add UnitTest project

---
+++ Vmr.Dependency2.UnitTests/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace Vmr.Dependency2.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
